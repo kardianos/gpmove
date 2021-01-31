2e1a855d02c51e3c07692904ea439a591988d502
Daniel Theophanes <kardianos@gmail.com>
2021-01-30 22:43:29 -0600

    add alignjson

--- main.go
+++ main.go
@@ -24,30 +24,11 @@ Step 5: Manually re-index the images.
 
 
 ---sidecar yml file---
-data@kardianos1 /d/r/p/s/s/2/01> cat 20180101_000253_2C6CF514.yml
 TakenAt: 2018-01-01T00:02:53Z
 TakenSrc: meta
-UID: pqnkwgd18ghabopp
+UID: ...
 Type: image
-Title: Beaverton / USA / 2018
 OriginalName: IMG_20171231_160253871
-PlaceSrc: meta
-Altitude: 25
-Lat: 45.448994
-Lng: -122.796295
-Year: 2017
-Month: 12
-Day: 31
-ISO: 640
-Exposure: 1/24
-FNumber: 1.7
-FocalLength: 4
-CameraSerial: ZY224JR2TL
-Details:
-  Keywords: beaverton, gold, google, oregon, usa
-CreatedAt: 2021-01-27T05:55:25.001738034Z
-UpdatedAt: 2021-01-27T05:55:26.606953386Z
-
 
 --- google photos json file ---
 {
@@ -196,51 +177,69 @@ func run(ctx context.Context) error {
 					return nil
 				}),
 			},
-		},
-		{
-			Name: "alignjson",
-			Flags: []*task.Flag{
-				{Name: "original", Default: "", Usage: "Original Folder"},
-			},
-			Action: task.ActionFunc(func(ctx context.Context, st *task.State, sc task.Script) error {
-				original := st.Get("original").(string)
+			{
+				Name:  "alignjson",
+				Usage: "Run on extracted google takeout folder, prior to import into photoprism.",
+				Flags: []*task.Flag{
+					{Name: "import", Default: "", Usage: "Import Folder"},
+					{Name: "dry", Default: false, Usage: "Dry run. Do nothing."},
+				},
+				Action: task.ActionFunc(func(ctx context.Context, st *task.State, sc task.Script) error {
+					im := st.Get("import").(string)
+					dry := st.Get("dry").(bool)
 
-				wr := os.Stderr
-				logf := func(f string, v ...interface{}) {
-					fmt.Fprintf(wr, f, v...)
-				}
-
-				origCleaner, err := newPathCleaner(original)
-				if err != nil {
-					return err
-				}
-				err = filepath.Walk(original, func(p string, info os.FileInfo, err error) error {
-					if info.IsDir() {
-						return nil
+					wr := os.Stderr
+					logf := func(f string, v ...interface{}) {
+						fmt.Fprintf(wr, f, v...)
 					}
+					err := filepath.Walk(im, func(p string, info os.FileInfo, err error) error {
+						if info.IsDir() {
+							return nil
+						}
+						if err != nil {
+							return err
+						}
+						ext := filepath.Ext(p)
+						if ext != ".json" {
+							return nil
+						}
+						logf("json found %s: ", p)
+
+						dir, filename := filepath.Split(p)
+						filenameNoJSON := strings.TrimSuffix(filename, ext)
+
+						secondExt := filepath.Ext(filenameNoJSON)
+						switch {
+						case len(secondExt) == 0:
+							logf("\n")
+							return nil
+						case len(secondExt) > 9:
+							logf("\tsecond ext too long %s\n", secondExt)
+							return nil
+						}
+
+						filenameNoExt := filenameNoJSON[:len(filenameNoJSON)-len(secondExt)]
+
+						moveTo := filepath.Join(dir, filenameNoExt+ext)
+
+						_, err = os.Stat(moveTo)
+						exists := !os.IsNotExist(err)
+						logf("\tMove To (exists: %v): %s\n", exists, moveTo)
+						if exists {
+							return nil
+						}
+						if dry {
+							return nil
+						}
+						return os.Rename(p, moveTo)
+					})
 					if err != nil {
 						return err
 					}
-					ext := filepath.Ext(p)
-					if ext != ".json" {
-						return nil
-					}
-					logf("json found %s\n", p)
-					scParts, err := origCleaner.Split(p)
-					if err != nil {
-						return err
-					}
-					
-
 					return nil
-				})
-				if err != nil {
-					return err
-				}
-				return nil
-			}),
+				}),
+			},
 		},
-	},
 	}
 
 	st := task.DefaultState()
